2c7aa62bd748c056accf305f1267ae0ed11fa45b
sv7y68rth8-hash <sv7y68rth8@privaterelay.appleid.com>
2026-08-28 19:05:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(self.registration.unregister().then(()=>self.clients.claim())));self.addEventListener('fetch',()=>{});
+self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(self.registration.unregister()));
